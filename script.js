c65030fcf2f864209bcfb22e58be47626c2506d0
Datta-070 <chinmaydatt003@gmail.com>
2023-11-10 18:36:10 +0530

    Yes, First JS Code Done

---
+++ script.js
@@ -0,0 +1,8 @@
+function action(){
+    let title= document.getElementById('title');
+    let btn= document.getElementById('btn');
+    btn.style.display= 'none';
+    let para= document.getElementById('para');
+    para.style.display= 'none';
+    title.innerText= "Thanks for Checking out 😎👌";
+}
